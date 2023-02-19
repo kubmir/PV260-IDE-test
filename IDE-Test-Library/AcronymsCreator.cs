43ffa2e6aed940cc7bcff096d6c3e6efd8d3f54b
miroslavkubus <433645@muni.cz>
2023-02-19 01:18:05 +0100

    feat: example function with NUnit test to test setup

---
+++ IDE-Test-Library/AcronymsCreator.cs
@@ -0,0 +1,48 @@
+﻿namespace IDE_Test_Library;
+
+public interface IAcronymsCreator
+{
+    string GetAcronymOfSentence(string sentence);
+}
+
+public interface IModificator
+{
+    string[] ModifyWords(string[] words);
+}
+
+public class AcronymsCreator : IAcronymsCreator
+{
+    private readonly char[] _separators;
+    private readonly IEnumerable<IModificator> _modifiers;
+
+    public AcronymsCreator(char[] separators, IEnumerable<IModificator> modifiers)
+    {
+        _separators = separators;
+        _modifiers = modifiers;
+    }
+
+    public AcronymsCreator(char[] separators) : this(separators, new List<IModificator>())
+    { }
+
+    public AcronymsCreator() : this(new char[] { ' ' })
+    { }
+
+    public string GetAcronymOfSentence(string sentence)
+    {
+        if (sentence == "" || sentence == null)
+            return "";
+
+        var words = sentence.Split(_separators);
+
+        foreach (var modifier in _modifiers)
+        {
+            words = modifier.ModifyWords(words);
+        }
+
+        return words
+            .Select(word => word.First().ToString())
+            .Aggregate((first, second) => first + second)
+            .ToUpper();
+    }
+}
+
---
+++ IDE-Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IDETest
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
