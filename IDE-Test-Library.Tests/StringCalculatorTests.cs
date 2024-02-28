6e8c15f011a080f4cf2234bb57dd36841f46c119
miroslavkubus <433645@muni.cz>
2024-02-28 16:51:43 +0100

    feat: update packages

---
+++ IDE-Test-Library.Tests/StringCalculatorTests.cs
@@ -0,0 +1,32 @@
+﻿namespace IDE_Test_Library.Tests;
+
+public class Tests
+{
+    private StringCalculator calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        calculator = new StringCalculator();
+    }
+
+    [Test]
+    public void Add_EmptyString_ReturnsZero()
+    {
+        var result = calculator.Add("");
+
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    [TestCase("", 0)]
+    [TestCase("5", 5)]
+    [TestCase("6", 6)]
+    [TestCase("6,5", 11)]
+    public void Add_OneNumber_ReturnTheSameNumber(String par, int expectedResult)
+    {
+        var result = calculator.Add(par);
+
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
---
+++ IDE-Test-Library/StringCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace IDE_Test_Library
+{
+	public class StringCalculator
+	{
+		public StringCalculator()
+		{
+		}
+
+		public int Add(String number)
+		{
+			var numbers = number.Split(",");
+			var result = 0;
+			foreach(var num in numbers)
+			{
+                var success = int.TryParse(num, out int realNumber);
+				result += realNumber;
+            }
+
+
+			return result;
+					}
+	}
+}
+
